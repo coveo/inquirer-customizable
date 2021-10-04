29226bfa49442a63359730c4c4fefb701a9a9643
Louis Bompart <lbompart@coveo.com>
2021-10-04 11:54:11 -0400

    feat: add radioText (#3)
    * add radioText
    
    * Update examples/radioText/example.ts
    
    Co-authored-by: Olivier Lamothe <olamothe@coveo.com>
    
    Co-authored-by: Olivier Lamothe <olamothe@coveo.com>

---
+++ examples/radioText/example.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+import { renderer } from "./radioTextRenderer";
+import { checkboxControls as controls } from "./radioTextControls";
+import {
+  CustomizablePromptAnswers,
+  CustomizablePrompt,
+} from "../../src/customizablePrompt";
+import chalk from "chalk";
+
+inquirer.registerPrompt("test", CustomizablePrompt);
+
+inquirer
+  .prompt<CustomizablePromptAnswers>([
+    {
+      type: "test",
+      name: "hi",
+      style: "radio",
+      keys: [
+        { displayName: "Sources", id: "sources" },
+        { displayName: "Fields", id: "fields" },
+        { displayName: "Pipelines", id: "pipelines" },
+      ],
+      values: [
+        { displayName: chalk`{green {bold A}dd}`, id: "add" },
+        { displayName: chalk`{cyan {bold E}dit}`, id: "edit" },
+        { displayName: chalk`{white {bold S}kip}`, id: "skip" },
+        { displayName: chalk`{red {bold D}elete}`, id: "delete" },
+      ],
+      controls,
+      renderer,
+    },
+  ])
+  .then((answers) =>
+    console.log(`Prompt session complete, here's the object that inquirer would return
+  ${JSON.stringify(answers)}`)
+  );
